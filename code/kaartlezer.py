753b17510bf6e2f59d0c15b0319613be148734b5
HVerschueren <r0842391@student.thomasmore.be>
2022-10-12 19:24:05 +0200

    uid kunnen aanpassen

--- code/kaartlezer.py
+++ code/kaartlezer.py
@@ -1,8 +1,6 @@
 import board
 import busio
 from digitalio import DigitalInOut
-
-
 from adafruit_pn532.spi import PN532_SPI
 
 # SPI connection:
@@ -16,12 +14,21 @@ print("Found PN532 with firmware version: {0}.{1}".format(ver, rev))
 # Configure PN532 to communicate with MiFare cards
 pn532.SAM_configuration()
 
+lastCardUID = 0
+
 print("Waiting for RFID/NFC card...")
 while True:
     # Check if a card is available to read
     uid = pn532.read_passive_target(timeout=0.5)
-    print(".", end="")
+    #print(".", end="")
     # Try again if no card is available.
     if uid is None:
         continue
-    print("Found card with UID:", [hex(i) for i in uid])
+    cardUID = [hex(i) for i in uid]#binairy naar hexadecimaal
+    if cardUID != lastCardUID:
+        #print(uid)
+        print(cardUID)
+        print(int.from_bytes(uid,"big"))#maakt van de binairy array een decimaal nummer
+        print()
+        
+        lastCardUID = cardUID
