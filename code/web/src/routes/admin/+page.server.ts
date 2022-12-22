cae56c0117bdf0010f5715617fb20135dfea281f
Jonas Claes <jonas@jonasclaes.be>
2022-12-22 11:21:09 +0100

    Fiscal improvements

--- code/web/src/routes/admin/+page.server.ts
+++ code/web/src/routes/admin/+page.server.ts
@@ -1,17 +1,62 @@
 import { invalid } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ platform }) => {
+export interface Report {
+	date: string;
+	amount: number;
+}
+
+export interface AdminPageData {
+	freeCoffeeAmount: number;
+	reports: Report[];
+}
+
+export const load: PageServerLoad = async ({ platform }): Promise<AdminPageData> => {
 	if (!platform) {
 		return {
-			freeCoffeeAmount: 10
+			freeCoffeeAmount: 10,
+			reports: [
+				{
+					date: '2023/01',
+					amount: 6
+				},
+				{
+					date: '2023/02',
+					amount: 1
+				},
+				{
+					date: '2022/10',
+					amount: 8
+				},
+				{
+					date: '2022/11',
+					amount: 9
+				},
+				{
+					date: '2022/12',
+					amount: 4
+				}
+			]
 		};
 	}
 
 	const freeCoffeeAmount = (await platform.env.SETTINGS.get('freeCoffeeAmount')) || '0';
 
+	const reportKeys = await platform.env.CARDS.list<string>({ prefix: 'monthly:' });
+	const reports: Report[] = [];
+
+	for (const reportKey of reportKeys.keys) {
+		const reportAmount = (await platform.env.CARDS.get(reportKey.name)) || '0';
+
+		reports.push({
+			date: reportKey.name.replace('monthly:', ''),
+			amount: parseInt(reportAmount)
+		});
+	}
+
 	return {
-		freeCoffeeAmount: parseInt(freeCoffeeAmount)
+		freeCoffeeAmount: parseInt(freeCoffeeAmount),
+		reports
 	};
 };
 
